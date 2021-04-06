24f1af3c103c8d76394b2e2083378dfa91b1e25b
alexanchek <58663068+alexanchek@users.noreply.github.com>
2021-04-06 15:51:46 +0300

    feat: common letters solved

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,23 +9,20 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
- function getCommonCharacterCount(s1, s2) {
-	let commonLetters = 0;
-  
-  s1 = s1.split('');
-  s2 = s2.split('');
-  
-  
-  
-  for (let i =0; i<s1.length; i++) {
-  
-  	if (s2.indexOf(s1[i]) != -1) {
-    	commonLetters++;
-    	s1.splice(i,1);
-      s2.splice(s2[s2.indexOf(s1[i])],1);
+function getCommonCharacterCount(s1, s2) {
+  let commonLetters = 0;
+  const sm1 = s1.split('');
+  const sm2 = s2.split('');
+  for (let i = 0; i < sm1.length; i++) {
+    const tempI = sm2.indexOf(sm1[i]);
+    if (tempI !== -1) {
+      commonLetters++;
+      sm2.splice(tempI, 1);
+      sm1.splice(i, 1);
       i--;
     }
   }
+
   return commonLetters;
 }
 
