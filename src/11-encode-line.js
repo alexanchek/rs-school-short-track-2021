18f8902d7a54d1bae002123a349008de320b0beb
alexanchek <58663068+alexanchek@users.noreply.github.com>
2021-04-07 10:02:23 +0300

    feat: encode list solved

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+
+function encodeLine(str) {
+  const letters = [];
+  let finalResult = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    letters[i] = [];
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      if (count !== 1) {
+        letters[i].push(count);
+      }
+      letters[i].push(str[i]);
+
+      count = 1;
+    }
+  }
+
+  letters.forEach((letter) => {
+    if (letter.length > 0) { finalResult += letter.join(''); }
+  });
+
+  return finalResult;
 }
 
 module.exports = encodeLine;
